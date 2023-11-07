f2b2e9acc6091bb50856bef9f6a58f0a9ac69776
shanerigsby <55967716+shanerigsby@users.noreply.github.com>
2023-11-07 11:19:00 -0600

    trying to understand..

--- functions/puzzles/[puzzle].js
+++ functions/puzzles/[puzzle].js
@@ -1,4 +1,15 @@
 export async function onRequest(context) {
-    var val = await context.env.NAMESPACE.get("first-key");
-    return new Response(`The puzzle: ${context.params.puzzle} has value: ${val}`);
-  }
+    var puzzle = validatePuzzle(context.params.puzzle);
+    if (puzzle) {
+        var val = await context.env.NAMESPACE.get(puzzle);
+        return new Response(`The puzzle: ${puzzle} has value: ${val}`);
+    }
+}
+
+function validatePuzzle(puzzle) {
+    const re = /^[a-zA-Z]{16}$/;
+    if (re.test(puzzle)) {
+        return puzzle.toLowerCase();
+    }
+    return null;
+}
