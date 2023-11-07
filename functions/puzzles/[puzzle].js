0f4c50f7a44da8b1f31028a3027380c544fcdc3c
shanerigsby <55967716+shanerigsby@users.noreply.github.com>
2023-11-07 11:25:28 -0600

    trying to understand..

--- functions/puzzles/[puzzle].js
+++ functions/puzzles/[puzzle].js
@@ -1,9 +1,16 @@
 export async function onRequest(context) {
     var puzzle = validatePuzzle(context.params.puzzle);
     if (puzzle) {
-        var val = await context.env.NAMESPACE.get(puzzle);
-        return new Response(`The puzzle: ${puzzle} has value: ${val}`);
+        var solution = await context.env.NAMESPACE.get(puzzle);
+        if (!solution) {
+            fetch(`https://wordfind.azurewebsites.net/api/solver?board=${puzzle}`)
+                .then(res => res.text())
+                .then(text => solution = text)
+                .catch(err => console.error(err));
+        }
+        return new Response(`The puzzle: ${puzzle} has solution: ${solution}`);
     }
+    return new Response();
 }
 
 function validatePuzzle(puzzle) {
