dbbe52316ad2b305c465066b60f9787cd27739ba
shanerigsby <55967716+shanerigsby@users.noreply.github.com>
2023-11-07 10:55:11 -0600

    improve kv route

--- functions/kv.js
+++ functions/kv.js
@@ -1,10 +1,7 @@
-export default {
-    async fetch(request, env, ctx) {
-      const value = await env.NAMESPACE.get("first-key");
-  
-      if (value === null) {
-        return new Response("Value not found", { status: 404 });
-      }
-      return new Response(value);
-    },
-  };
+export async function onRequest(request, env, ctx) {
+    const value = await env.NAMESPACE.get("first-key");
+    if (value === null) {
+      return new Response("Value not found", { status: 404 });
+    }
+    return new Response(value);
+}
